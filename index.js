acbdd632452b68df13063d9b093197df61b2fe48
unknown <montasirazad@gmail.com>
2022-04-23 16:49:17 +0600

    finished

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
 app.use(cors());
 app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-/// user name : azad1403
-/// pass: 1tRtV7xUk38Vvpd4
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k8hkv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -25,8 +25,33 @@ async function run() {
         app.post('/service', async (req, res) => {
             const service = req.body;
             const result = await dbCollection.insertOne(service);
-            console.log(result);
             res.json(result)
+        })
+
+        // Get all data api
+        app.get('/services', async (req, res) => {
+            const cursor = dbCollection.find({})
+            const services = await cursor.toArray();
+            res.send(services);
+            console.log(services);
+        })
+        // Find a single data 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await dbCollection.findOne(query);
+            res.json(result);
+            console.log(result);
+        });
+
+        // DELETE API
+
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id :ObjectId(id)};
+            const result = await dbCollection.deleteOne(query);
+            console.log(result);
+            res.json(result);
         })
 
     } finally {
